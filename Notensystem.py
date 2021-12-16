1e8bc6a9ac37cb11ba07e976ac5356179a71ebb0
dljsinehiekndhvimejjvjcjcdjdj <96227925+dljsinehiekndhvimejjvjcjcdjdj@users.noreply.github.com>
2021-12-16 09:15:39 +0100

    Delete Notensystem.py

--- Notensystem.py
+++
@@ -1,9 +0,0 @@
-Note = float(input("Gib deine Note ein: "))
-if Note >= 5.5:
-    print("Deine Note im amerikanischen System ist A")
-elif Note < 5.5 and Note >= 4.5:
-    print("Deine Note im amerikanischen System ist B")
-elif Note < 4.5 and Note >= 4.0:
-    print("Deine Note im amerikanischen System ist C")
-else:
-    print("Deine Note im amerikanischen System ist F")
